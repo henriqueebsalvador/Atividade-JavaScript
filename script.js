e3ba001ac06da2178f2e412aca8b92c9c1c4ee83
henrique <henrique_b_salvador@estudante.sesisenai.org.br>
2026-03-24 11:18:53 -0300

    Atividade 1

---
+++ script.js
@@ -0,0 +1,11 @@
+let soma = 0;
+let numero;
+
+while (true) {
+    numero = prompt("Insira um número (ou digite 'parar' para parar)");
+    if (numero === "parar") {
+        break;
+    }
+    soma += Number(numero);
+}
+console.log(soma);
